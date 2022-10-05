395b2fc1c0ed636cdb1122df00310511d90c8f18
krst221 <113525346+krst221@users.noreply.github.com>
2022-10-06 00:13:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let rival1 = '';
 let rival2 = '';
 let firstTurn = true;
 let firstAttacked = false;
+let gameOver = false;
 
 const getCharacters = async () => {
     const res = await fetch('http://localhost:3000/characters');
@@ -22,7 +23,6 @@ const getCharacters = async () => {
 }
 
 const printCharacters = (characters) => {
-    console.log(characters);
     for(const character of characters) {
         const {avatar, critic, damage, defense, vitality, name} = character;
         vitalities.push(vitality);
@@ -44,24 +44,27 @@ const printStats = (character, div) => {
     const {damage, defense, vitality} = character;
     const dmgf = damage;
     const dmg$$ = document.createElement('p');
-    const vari$$ = document.createElement('p');
+    const avg$$ = document.createElement('p');
     const def$$ = document.createElement('p');
     const vit$$ = document.createElement('p');
     let dmgt = 0;
-    let vari = 0;
+    let avgt = 0;
+    let avg = 0;
     for (let i = 0 ; i < dmgf.length ; i++) {
         const dpos = dmgf[i].indexOf('d');
         const mult = dmgf[i].substring(0 ,dpos);
         const dmg = dmgf[i].substring(dpos + 1, dmgf[i].length);
+        avg = dmg * 0.583;
         dmgt += mult*dmg;
-        vari += mult*1;
+        avgt += mult*avg;
     }
+    avgt = avgt.toFixed(0);
     dmg$$.textContent = 'Max damage: ' + dmgt;
-    vari$$.textContent = 'Damage variance: ' +vari;
+    avg$$.textContent = 'Average damage: ' + avgt;
     def$$.textContent = 'Defense: ' + defense;
     vit$$.textContent = 'Vitality: ' + vitality;
     div.appendChild(dmg$$);
-    div.appendChild(vari$$);
+    div.appendChild(avg$$);
     div.appendChild(def$$);
     div.appendChild(vit$$);
 }
@@ -73,7 +76,15 @@ const charSelected = (characters, name, div) => {
         select$$.textContent = ('You choose ' + name + ' as your first fighter! Please pick the second one!');
         selectedChars++;
     }
-    else if(rival1 === name) select$$.textContent = ('You choose ' + name + ' again! Please pick a differenet fighter!');
+    else if(rival1 === name) {
+        select$$.textContent = ('You choose ' + name + ' again! Please pick a different fighter!');
+        for(let i = 0 ; i < characters.length; i++) {
+            if(characters[i].name !== name) {
+                const char = chars$$.childNodes[i];
+                char.classList.remove('selected');
+            }
+        }
+    }   
     else {
         if(rival2 !== '') {
             for(let i = 0 ; i < characters.length; i++) {
@@ -103,76 +114,77 @@ const startFight = (characters, rival1, rival2) => {
 }
 
 const calcTurn = (characters, r1i, r2i) => {
-    if(firstTurn) {
-        turn = 1;
-        const turn$$ = document.createElement('h2');
-        while (arena$$.firstChild) arena$$.removeChild(arena$$.lastChild);
-        turn$$.textContent = 'Turno ' + turn;
-        arena$$.appendChild(turn$$);
-        let first = Math.floor(Math.random() * 2);
-        if(first === 0) {
-            const atk$$ = document.createElement('p');
-            atk$$.textContent = characters[r1i].name + ' va a atacar a ' + characters[r2i].name + '!';
-            const vida$$ = document.createElement('p');
-            vida$$.textContent = 'A ' + characters[r2i].name + ' le quedan ' + characters[r2i].vitality + ' puntos de vida!';
-            setTimeout(() => {arena$$.appendChild(atk$$)}, 500);
-            setTimeout(() => {arena$$.appendChild(vida$$)}, 1500);
-            setTimeout(() => {makeDamage(characters, r1i, r2i)}, 1500); 
-            firstAttacked = true;
+    if(!gameOver) {
+        if(firstTurn) {
+            turn = 1;
+            const turn$$ = document.createElement('h2');
+            while (arena$$.firstChild) arena$$.removeChild(arena$$.lastChild);
+            turn$$.textContent = 'Turno ' + turn;
+            arena$$.appendChild(turn$$);
+            let first = Math.floor(Math.random() * 2);
+            if(first === 0) {
+                const atk$$ = document.createElement('p');
+                atk$$.textContent = characters[r1i].name + ' va a atacar a ' + characters[r2i].name + '!';
+                const vida$$ = document.createElement('p');
+                vida$$.textContent = 'A ' + characters[r2i].name + ' le quedan ' + characters[r2i].vitality + ' puntos de vida!';
+                setTimeout(() => {arena$$.appendChild(atk$$)}, 300);
+                setTimeout(() => {arena$$.appendChild(vida$$)}, 1000);
+                setTimeout(() => {makeDamage(characters, r1i, r2i)}, 1000); 
+                firstAttacked = true;
+            }
+            else {
+                const atk$$ = document.createElement('p');
+                atk$$.textContent = characters[r2i].name + ' va a atacar a ' + characters[r1i].name + '!';
+                const vida$$ = document.createElement('p');
+                vida$$.textContent = 'A ' + characters[r1i].name + ' le quedan ' + characters[r1i].vitality + ' puntos de vida!';
+                setTimeout(() => {arena$$.appendChild(atk$$)}, 300);
+                setTimeout(() => {arena$$.appendChild(vida$$)}, 1000);
+                setTimeout(() => {makeDamage(characters, r2i, r1i)}, 1000); 
+                firstAttacked = false;
+            }
+            firstTurn = false;
+            turn++;
         }
         else {
-            const atk$$ = document.createElement('p');
-            atk$$.textContent = characters[r2i].name + ' va a atacar a ' + characters[r1i].name + '!';
-            const vida$$ = document.createElement('p');
-            vida$$.textContent = 'A ' + characters[r1i].name + ' le quedan ' + characters[r1i].vitality + ' puntos de vida!';
-            setTimeout(() => {arena$$.appendChild(atk$$)}, 500);
-            setTimeout(() => {arena$$.appendChild(vida$$)}, 1500);
-            setTimeout(() => {makeDamage(characters, r2i, r1i)}, 1500); 
-            firstAttacked = false;
+            const turn$$ = document.createElement('h2');
+            turn$$.textContent = 'Turno ' + turn;
+            arena$$.appendChild(turn$$);
+            if(!firstAttacked) {
+                const atk$$ = document.createElement('p');
+                atk$$.textContent = characters[r1i].name + ' va a atacar a ' + characters[r2i].name + '!';
+                const vida$$ = document.createElement('p');
+                vida$$.textContent = 'A ' + characters[r2i].name + ' le quedan ' + characters[r2i].vitality + ' puntos de vida!';
+                setTimeout(() => {arena$$.appendChild(atk$$)}, 300);
+                setTimeout(() => {arena$$.appendChild(vida$$)}, 1000);
+                setTimeout(() => {makeDamage(characters, r1i, r2i)}, 1000); 
+                firstAttacked = true; 
+            }   
+            else {
+                const atk$$ = document.createElement('p');
+                atk$$.textContent = characters[r2i].name + ' va a atacar a ' + characters[r1i].name + '!';
+                const vida$$ = document.createElement('p');
+                vida$$.textContent = 'A ' + characters[r1i].name + ' le quedan ' + characters[r1i].vitality + ' puntos de vida!';
+                setTimeout(() => {arena$$.appendChild(atk$$)}, 300);
+                setTimeout(() => {arena$$.appendChild(vida$$)}, 1000);
+                setTimeout(() => {makeDamage(characters, r2i, r1i)}, 1000); 
+                firstAttacked = false;
+            }
+            turn++;
         }
-        firstTurn = false;
-        turn++;
+        setTimeout(() => {while (arena$$.firstChild) {arena$$.removeChild(arena$$.lastChild);}}, 6000);
+        setTimeout(() => {calcTurn(characters, r1i, r2i)}, 6000);
     }
-    else {
-        const turn$$ = document.createElement('h2');
-        turn$$.textContent = 'Turno ' + turn;
-        arena$$.appendChild(turn$$);
-        if(!firstAttacked) {
-            const atk$$ = document.createElement('p');
-            atk$$.textContent = characters[r1i].name + ' va a atacar a ' + characters[r2i].name + '!';
-            const vida$$ = document.createElement('p');
-            vida$$.textContent = 'A ' + characters[r2i].name + ' le quedan ' + characters[r2i].vitality + ' puntos de vida!';
-            setTimeout(() => {arena$$.appendChild(atk$$)}, 500);
-            setTimeout(() => {arena$$.appendChild(vida$$)}, 1500);
-            setTimeout(() => {makeDamage(characters, r1i, r2i)}, 1500); 
-            firstAttacked = true; 
-        }   
-        else {
-            const atk$$ = document.createElement('p');
-            atk$$.textContent = characters[r2i].name + ' va a atacar a ' + characters[r1i].name + '!';
-            const vida$$ = document.createElement('p');
-            vida$$.textContent = 'A ' + characters[r1i].name + ' le quedan ' + characters[r1i].vitality + ' puntos de vida!';
-            setTimeout(() => {arena$$.appendChild(atk$$)}, 500);
-            setTimeout(() => {arena$$.appendChild(vida$$)}, 1500);
-            setTimeout(() => {makeDamage(characters, r2i, r1i)}, 1500); 
-            firstAttacked = false;
-        }
-        turn++;
-    }
-    if (characters[r1i].vitality > 0 && characters[r2i].vitality > 0) {
-        setTimeout(() => {while (arena$$.firstChild) {arena$$.removeChild(arena$$.lastChild);}}, 8000);
-        setTimeout(() => {calcTurn(characters, r1i, r2i)}, 8000);
-    }
-    else setTimeout(() => {drawTheWinner(characters, r1i, r2i)}, 8000);
+    else drawTheWinner(characters, r1i, r2i);
 }
 
 const makeDamage = (characters, atk, def) => {
     const damage$$ = document.createElement('p');
     let damage = 0;
-    setTimeout(() => {damage = calculateDamage(characters, atk) - characters[def].defense; if(damage < 0) damage = 0}, 700); 
+    setTimeout(() => {damage = calculateDamage(characters, atk) - characters[def].defense; 
+        if(damage < 0) damage = 0;}, 500); 
     setTimeout(() => {characters[def].vitality = characters[def].vitality - damage;
         if(characters[def].vitality > 0) damage$$.textContent = characters[atk].name + ' acaba de quitar ' + damage + ' puntos de vida a ' + characters[def].name + '! A ' + characters[def].name + ' le quedan ' + characters[def].vitality + ' puntos de vida!';
-        else damage$$.textContent = characters[atk].name + ' acaba de quitar ' + damage + ' puntos de vida a ' + characters[def].name + '! A ' + characters[def].name + ' no le quedan puntos de vida!';}, 4000);
+        else {damage$$.textContent = characters[atk].name + ' acaba de quitar ' + damage + ' puntos de vida a ' + characters[def].name + '! A ' + characters[def].name + ' no le quedan puntos de vida!'; gameOver = true;}}, 1800);
     setTimeout(() => {arena$$.appendChild(damage$$)}, 2500);
 }
 
@@ -192,8 +204,10 @@ const calculateDamage = (characters, atk) => {
             dmg = dmg * 2;
             dmg$$.textContent = dmg + ' de daño! GOLPE CRITICO!!';
         }
-        else dmg$$.textContent = dmg + ' de daño!';
-        setTimeout(() => {arena$$.appendChild(dmg$$)}, 500*(i+1));
+        else {
+            dmg$$.textContent = dmg + ' de daño!';
+        }
+        setTimeout(() => {arena$$.appendChild(dmg$$)}, 300*(i+1));
         damage += dmg;
     }
     return damage;
@@ -218,10 +232,16 @@ const resetGame = () => {
     rival1 = '';
     rival2 = '';
     firstTurn = true;
+    firstAttacked = false;
+    gameOver = false;
     for(let i = 0 ; i < characters.length ; i++) {
         characters[i].vitality = vitalities[i];
+        const char = chars$$.childNodes[i];
+        char.classList.remove('selected');
     }
     while (arena$$.firstChild) arena$$.removeChild(arena$$.lastChild);
+    select$$.textContent = 'Choose your fighters!';
+    arena$$.appendChild(select$$);
 }
 
 getCharacters();
